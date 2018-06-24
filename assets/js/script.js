c42d490681b3d6006f2412a2689306e2aadb92e5
cyril jardin <cyilroc60@hotmail.fr>
2018-06-24 13:05:29 +0200

    p2js4

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function fun(){
+//declaration de variable
+ var lastname = document.getElementById('lastname').value='';
+ var firstname = document.getElementById('firstname').value='';
+ var city = document.getElementById('city').value='';
+}
